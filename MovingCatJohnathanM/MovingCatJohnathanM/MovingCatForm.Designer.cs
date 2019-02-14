f19fe829a383b034952ab8ac79f7bae762507629
Johnathan Meloche <johnathan.meloche@ocsbstudent.ca>
2019-02-14 13:29:42 -0500

    Started Moving Cat

---
+++ MovingCatJohnathanM/MovingCatJohnathanM/MovingCatForm.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace MovingCatJohnathanM
+{
+    partial class frmMovingCat
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMovingCat));
+            this.picZombie = new System.Windows.Forms.PictureBox();
+            this.btnForwards = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picZombie)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picZombie
+            // 
+            this.picZombie.BackColor = System.Drawing.Color.Transparent;
+            this.picZombie.Image = global::MovingCatJohnathanM.Properties.Resources.zombie1;
+            this.picZombie.InitialImage = null;
+            this.picZombie.Location = new System.Drawing.Point(64, 31);
+            this.picZombie.Name = "picZombie";
+            this.picZombie.Size = new System.Drawing.Size(192, 193);
+            this.picZombie.TabIndex = 0;
+            this.picZombie.TabStop = false;
+            this.picZombie.Click += new System.EventHandler(this.picZombie_Click);
+            // 
+            // btnForwards
+            // 
+            this.btnForwards.Location = new System.Drawing.Point(300, 106);
+            this.btnForwards.Name = "btnForwards";
+            this.btnForwards.Size = new System.Drawing.Size(125, 30);
+            this.btnForwards.TabIndex = 2;
+            this.btnForwards.Text = "Click to make him run";
+            this.btnForwards.UseVisualStyleBackColor = true;
+            this.btnForwards.Click += new System.EventHandler(this.btnForwards_Click);
+            // 
+            // frmMovingCat
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(457, 264);
+            this.Controls.Add(this.btnForwards);
+            this.Controls.Add(this.picZombie);
+            this.Name = "frmMovingCat";
+            this.Text = "Moving Cat by Johnathan M";
+            this.Load += new System.EventHandler(this.frmMovingCat_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picZombie)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        public System.Windows.Forms.PictureBox picZombie;
+        public System.Windows.Forms.Button btnForwards;
+    }
+}
+
---
+++ MovingCatJohnathanM/MovingCatJohnathanM/MovingCatForm.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/*
+ * Created by: Johnathan Meloche
+ * Created on: 14/02/19
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #9 Moving Cat
+ * This program changes the image to the next picture when a button is clicked
+*/
+namespace MovingCatJohnathanM
+{
+    public partial class frmMovingCat : Form
+    {
+        public frmMovingCat()
+        {
+            InitializeComponent();
+        }
+
+        private void btnForwards_Click(object sender, EventArgs e)
+        {
+            if (picZombie.Image == Properties.Resources.zombie1)
+            {
+                this.picZombie.Image = Properties.Resources.zombie2;
+            }
+            else if (picZombie.Image == Properties.Resources.zombie2)
+            {
+                this.picZombie.Image = Properties.Resources.zombie3;
+            }
+            else if (picZombie.Image == Properties.Resources.zombie3)
+            {
+                this.picZombie.Image = Properties.Resources.zombie4;
+            }
+            else if (picZombie.Image == Properties.Resources.zombie4)
+            {
+                this.picZombie.Image = Properties.Resources.zombie5;
+            }
+            else if (picZombie.Image == Properties.Resources.zombie5)
+            {
+                this.picZombie.Image = Properties.Resources.zombie6;
+            }
+            else if (picZombie.Image == Properties.Resources.zombie6)
+            {
+                this.picZombie.Image = Properties.Resources.zombie7;
+            }
+            else if (picZombie.Image == Properties.Resources.zombie7)
+            {
+                this.picZombie.Image = Properties.Resources.zombie8;
+            }
+            else if (picZombie.Image == Properties.Resources.zombie8)
+            {
+                this.picZombie.Image = Properties.Resources.zombie9;
+            }
+            else if (picZombie.Image == Properties.Resources.zombie9)
+            {
+                this.picZombie.Image = Properties.Resources.zombie10;
+            }
+            else if (picZombie.Image == Properties.Resources.zombie10)
+            {
+                this.picZombie.Image = Properties.Resources.zombie1;
+            }
+        }
+
+        private void picZombie_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmMovingCat_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
