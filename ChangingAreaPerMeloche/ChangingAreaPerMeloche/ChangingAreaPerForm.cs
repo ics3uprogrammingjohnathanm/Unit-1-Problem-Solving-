69b43c2c156b959c5e7c761b8131df99764dd486
Johnathan Meloche <johnathan.meloche@ocsbstudent.ca>
2019-02-16 00:49:25 -0500

    Started ChangingAreaPer

--- ChangingAreaPerMeloche/ChangingAreaPerMeloche/ChangingAreaPerForm.cs
+++ ChangingAreaPerMeloche/ChangingAreaPerMeloche/ChangingAreaPerForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace ChangingAreaPerMeloche
 {
-    public partial class Form1 : Form
+    public partial class frmChangingAreaPer : Form
     {
-        public Form1()
+        public frmChangingAreaPer()
         {
             InitializeComponent();
         }
