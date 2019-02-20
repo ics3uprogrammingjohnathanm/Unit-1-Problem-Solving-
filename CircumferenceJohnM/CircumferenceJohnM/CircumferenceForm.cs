6b0892963c8d5b0baa21b277d6225569caa108a5
Johnathan Meloche <johnathan.meloche@ocsbstudent.ca>
2019-02-20 12:37:38 -0500

    Started Circumeference

---
+++ CircumferenceJohnM/CircumferenceJohnM/CircumferenceForm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CircumferenceJohnM
+{
+    public partial class frmCircumference : Form
+    {
+        const double PI = Math.PI;
+        public frmCircumference()
+        {
+            InitializeComponent();
+        }
+
+        private void frmCircumference_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
